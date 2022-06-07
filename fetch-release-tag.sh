4f59899403d37afc2968743d448651c28a093324
Manoj VM <manoj@fynarfin.io>
2022-06-07 23:03:04 +0530

    release-tag-to-env-var-bash

---
+++ fetch-release-tag.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+nmln=`grep  "name:" helm/ph-ee-engine/Chart.yaml`
+verln=`grep  "version:" helm/ph-ee-engine/Chart.yaml`
+nm=`echo $nmln |cut -d ' ' -f 2`
+ver=`echo $verln |cut -d ' ' -f 2`
+export phee-engine-release_tag="$nm-$ver"
