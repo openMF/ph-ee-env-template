323fda17c72f9fd3e8ad7b9941d665e91081443d
Manoj VM <manoj@fynarfin.io>
2022-06-07 23:21:51 +0530

    release-tag-to-env-var-bash1

--- fetch-release-tag.sh
+++ fetch-release-tag.sh
@@ -5,4 +5,4 @@ nm=`echo $nmln |cut -d ' ' -f 2`
 ver=`echo $verln |cut -d ' ' -f 2`
 phee_engine_release_tag=`echo $nm-$ver`
 export phee_engine_release_tag
-echo "done"
+echo $phee_engine_release_tag
